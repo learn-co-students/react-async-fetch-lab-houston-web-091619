dc46a2b8ebc73826ac7f81a1c7d406fd453ae511
Israel <Israel.pro1994@gmail.com>
2019-11-21 10:30:25 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React from 'react';
+
+class App extends React.Component{
+
+    state={
+        team: []
+    }
+
+    
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(astros => 
+            this.setState({
+                team: astros
+            })
+        )
+    }
+
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default App;
